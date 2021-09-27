0b8798083eccd34958be6366a1684ea21b4cd3a4
avinash uttamchandani <avinash@nonholonomy.com>
2021-09-27 17:59:53 -0400

    test decoder changes from class

--- 01_game_of_life/test_decoders.sv
+++ 01_game_of_life/test_decoders.sv
@@ -9,6 +9,42 @@ module test_decoders;
   wire [7:0] out;
 
   decoder_3_to_8 UUT(ena, in, out);
+  
+  logic input_1_2;
+  wire [1:0] output_1_2;
+  decoder_1_to_2 UUT_1_2(
+    .ena(ena),
+    .in(input_1_2),
+    .out(output_1_2)
+  );
+  
+  always begin
+    if ((ena == 0) && (output_1_2 !== 2'b0)) begin
+      $fatal("ena should turn off outputs!!!");
+    end
+  end
+  
+  initial begin : testing_1_2_decoder
+    $dumpfile("decoders.vcd");
+    $dumpvars(0, UUT_1_2);
+    ena = 1;
+    $display("[d1:2] ena in | out");
+    for (int i = 0; i < 2; i = i + 1) begin
+      input_1_2 = i[0];
+      #1 $display("[d1:2] %1b %1b | %2b", ena, input_1_2, output_1_2);
+    end
+
+    ena = 0;
+    for (int i = 0; i < 2; i = i + 1) begin
+      input_1_2 = i[0];
+      #1 $display("[d1:2] %1b %1b | %2b", ena, input_1_2, output_1_2);
+    end
+        
+    // $finish;      
+    
+  end
+
+  /*
 
   initial begin
     // Collect waveforms
@@ -19,16 +55,16 @@ module test_decoders;
     $display("ena in | out");
     for (int i = 0; i < 8; i = i + 1) begin
       in = i[2:0];
-      #1 $display("%1b %2b | %4b", ena, in, out);
+      #2 $display("%1b %2b | %4b", ena, in, out);
     end
 
     ena = 0;
     for (int i = 0; i < 8; i = i + 1) begin
       in = i[2:0];
-      #1 $display("%1b %2b | %4b", ena, in, out);
+      #2 $display("%1b %2b | %4b", ena, in, out);
     end
         
     $finish;      
 	end
-
+*/
 endmodule
